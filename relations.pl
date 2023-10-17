5125299a27bb49d47beca28ad3cbc6234b37fda9
julia <148009758+P4R4D1GMSH1FT@users.noreply.github.com>
2023-10-17 19:03:48 +0200

    Add files via upload

---
+++ relations.pl
@@ -0,0 +1,18 @@
+/* simple relations, fictional and non-fictional;) just for run */
+female(julia).
+likes(julia, math).
+partner(dablju, julia).
+play(julia, dablju, starcraft).
+king(john, poland).
+human(aristotle).
+human(socrates).
+athenian(socrates).
+
+?- likes(julia,math).
+?- athenian(aristotle).
+?- king(john, poland).
+?- male(julia).
+?- play(julia, dablju, diablo).
+?- likes(julia, math).
+?- partner(dablju, julia).
+?- human(socrates).
